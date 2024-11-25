843df12ccb127f78547ee557f6b45b9070aaab0d
McTintor <igortintor99@gmail.com>
2024-11-25 17:02:02 +0100

    added dynamic routing and finished app logic

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const messages = [
     {
+      messageId: Math.random(),
       text: "Hi there!",
       user: "Amando",
       added: new Date()
     },
     {
+      messageId: Math.random(),
       text: "Hello World!",
       user: "Charles",
       added: new Date()
@@ -27,10 +29,24 @@ exports.getAddNewMessage = (req, res, next) => {
 } 
 
 exports.postMessage = (req, res, next) => {
+        const messageId = Math.random();
         const user = req.body.user;
         const text = req.body.messageText;
 
-        messages.push({ text, user, added: new Date() });
+        messages.push({ messageId, text, user, added: new Date() });
 
         res.redirect('/');
+}
+
+exports.getMessage = (req, res, next) => {
+    const targetMessageId = req.params.messageId;
+    const targetMessage = messages.find(message => message.messageId.toString() === targetMessageId);
+
+    res.render('details', {
+        messageId: targetMessage.messageId,
+        text: targetMessage.text,
+        user: targetMessage.user,
+        added: targetMessage.added,
+        pageTitle: 'Details'
+    })
 }
